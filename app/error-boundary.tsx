cf56f3dcc66a55568625145e83dc3c8f5537edb3
Maggie Liu <maggie.j.liu@gmail.com>
2023-06-13 15:28:54 -0700

    initial commmit

---
+++ app/error-boundary.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // Example "componentStack":
+    //   in ComponentThatThrows (created by App)
+    //   in ErrorBoundary (created by App)
+    //   in div (created by App)
+    //   in App
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <div>an error has been caught</div>;
+    }
+
+    return this.props.children;
+  }
+}
